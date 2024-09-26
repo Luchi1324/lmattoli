31262cf1855ae4958c2dd85eba30ad94cf285331
Luciano Mattoli <luchileigh8@gmail.com>
2024-09-26 10:47:19 -0400

    Refactor w/ class structure

--- kattis/solveEverywhere.hs
+++ kattis/solveEverywhere.hs
@@ -1,7 +1,29 @@
-countDistinctCities :: [String] -> Int
-countDistinctCities = undefined
+import Data.List (nub)
 
-main :: IO ()
-main = do
-    let testInput = ["boston", "cleveland", "tokyo", "bejing", "milan"]
-    print (countDistinctCities testInput)
+
+type City = String
+type TestCase = [City]     -- [String]
+type Problem = [TestCase]  -- [[String]]
+type Solution = [Int]
+
+solveEverywhere :: String -> String
+solveEverywhere = showResult . doTheWork . parseInput
+
+parseInput :: String -> Problem
+parseInput = readCases . tail . lines        -- lines takes in a string and splits it into a list of strings based on \n
+    where
+        readCases :: [String] -> [[String]]
+        readCases [] = []
+        readCases (x:xs) = let n = read x in
+            take n xs : readCases (drop n xs)
+
+doTheWork :: Problem -> [Int]
+doTheWork = map solveOne
+    where
+        solveOne :: [String] -> Int
+        solveOne cities = length (nub cities)
+
+showResult :: Solution -> String
+showResult = unlines . map show              -- unlines takes a list of strings and concat them into a single string, inserting a \n between each original string
+
+
